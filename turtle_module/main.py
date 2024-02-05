e9fba5e1fe6e5c7ee337e2d8c787e754ff69ab81
Your GitHub Username <joyhe520@gmail.com>
2024-02-05 14:20:04 -0600

    turtle different shape

--- turtle_module/main.py
+++ turtle_module/main.py
@@ -1,16 +1,47 @@
-from turtle import Turtle, Screen
+#? ------------- turtle square ----------------
+# from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("pink")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
+# timmy_the_turtle = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("pink")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.left(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.left(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.left(90)
+# timmy_the_turtle.forward(100)
 
 
+
+
+#? ------------- import modules with aliases ----------------
+import turtle as t
+from turtle import Screen
+
+tim = t.Turtle()
+
+#? ------------- draw dashed line ----------------
+# for _ in range(10):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+
+
+
+#? ------------- draw different shapes ----------------
+import random
+colors= ['yellow', 'red', 'blue', 'green']
+def draw_shape(num_sides):
+    for _ in range(num_sides):
+        angle = 360 / num_sides
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)  
+    
 screen = Screen()
 screen.exitonclick()
