15e6d3da0ef428c4e23e28cb47280405570affcd
Your GitHub Username <joyhe520@gmail.com>
2024-02-05 13:55:50 -0600

    turtle square

--- turtle_module/main.py
+++ turtle_module/main.py
@@ -3,6 +3,14 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("pink")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+
 
 screen = Screen()
 screen.exitonclick()
